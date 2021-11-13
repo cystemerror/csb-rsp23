88156f1dbc2fd6f21c49c405e5336b30820f7d2a
Code Sandbox <hello@codesandbox.io>
2021-11-13 04:09:24 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.43d4ec70a69f55053b1446f32dd0b2cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cf1ecdd00543a539bde",
+    "url": "https://cystemerror.github.io/csb-rsp23/static/css/main.7cc4756e.chunk.css"
+  },
+  {
+    "revision": "8cf1ecdd00543a539bde",
+    "url": "https://cystemerror.github.io/csb-rsp23/static/js/main.ea93afcf.chunk.js"
+  },
+  {
+    "revision": "52522712310cd7020473",
+    "url": "https://cystemerror.github.io/csb-rsp23/static/js/runtime~main.c14d7c61.js"
+  },
+  {
+    "revision": "b03751d2205a72ec14c7",
+    "url": "https://cystemerror.github.io/csb-rsp23/static/js/2.17d5301c.chunk.js"
+  },
+  {
+    "revision": "e907d1844c8622d28cd6fd7cb0d5c9fb",
+    "url": "https://cystemerror.github.io/csb-rsp23/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "https://cystemerror.github.io/csb-rsp23/precache-manifest.43d4ec70a69f55053b1446f32dd0b2cf.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("https://cystemerror.github.io/csb-rsp23/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
